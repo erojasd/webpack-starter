eb02e09cc4b4cfc76382561c0fa0eed20539f5bc
Ernesto <eirojasdiaz@gmail.com>
2020-07-05 00:07:36 -0400

    WebPack test Initial

---
+++ src/index.js
@@ -0,0 +1,6 @@
+ import { saludar } from './js/componentes' //por defecto busca un archivo con extensión .js
+ import './styles.css';
+
+ const nombre = 'Usuario'
+
+ saludar(nombre);
---
+++ webpack.prod.js
@@ -0,0 +1,77 @@
+/**importaciones */
+const HtmlWebPackPlugin = require('html-webpack-plugin'); //require:forma de node para cargar archivos de otros paquetes.
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+
+    optimization: {
+        minimizer: [new OptimizeCssAssetsPlugin()]
+    },
+    output: {
+        filename: 'app.[contentHash].js'
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                    minimize: false //en true minimiza el html
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        esModule: false
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contentHash].css', // '[name].[contentHash].css' para evitar el almacenamiento en caché
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets' }
+            ],
+        }),
+        new MinifyPlugin()
+    ]
+}
